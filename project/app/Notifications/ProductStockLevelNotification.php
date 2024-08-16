8b8de436275f4bc453dcc4477411c1fb85530fcc
Henrique3040 <henrique.de.sousa.lima.cardoso@student.ehb.be>
2024-08-16 17:44:20 +0200

    Feat: Notification
    Back end van de notification is gedaan
    User krijgen een email wanner producten onder 5 eenheden zijn!
    
    reviewd by: Henrique

--- project/app/Notifications/ProductStockLevelNotification.php
+++ project/app/Notifications/ProductStockLevelNotification.php
@@ -6,19 +6,25 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use App\Models\Producten;
+
 
 class ProductStockLevelNotification extends Notification
 {
     use Queueable;
+
+    protected $product;
+    protected $level;
 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Producten $product, $level)
     {
-        //
+        $this->product = Producten::find($product->id);
+        $this->level = 'under';
     }
 
     /**
@@ -39,12 +45,17 @@ class ProductStockLevelNotification extends Notification
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
+   {
+    $subject = $this->level == 'over' ? 'Product Stock Above 5 Units' : 'Product Stock Below 5 Units';
+
+    return (new MailMessage)
+                ->subject($subject)
+                ->greeting('Hello!')
+                ->line("The stock level for the product '{$this->product->product_name}' has gone {$this->level} 5 units.")
+                ->line("Current stock level: {$this->product->quantity}")
+                ->action('View Product', url('/producten/'.$this->product->id))
+                ->line('Thank you for using our application!');
+   }
 
     /**
      * Get the array representation of the notification.
